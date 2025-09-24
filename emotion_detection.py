9caed57948a7de4933d3ceb31ff75c1283df8a86
jonathancrawford05 <jonathancrawford05@gmail.com>
2025-09-24 06:12:31 -0400

    Update emotion_detection method to structure response

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,13 +1,18 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze: str)->str:
-    '''Get response text from the emotion detection model API endpoint
+    '''Extract emotions with scores from the emotion detection model API endpoint.
+
+    Use input text to call emotion detection endpoint and return a dictionary
+    containing the emotions as keys and scores as values.
 
     Args:
         text_to_analyze (str): Input text to be sent for emotion detection
 
     Outputs:
-        (str): response objects text attribute's value
+        (dict): Dictionary with emotions and their score the
+                last key value pair highlights top scoring emotion
     '''
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -17,5 +22,13 @@ def emotion_detector(text_to_analyze: str)->str:
         }
     }
     response = requests.post(url, json = myobj, headers=header)
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+    # Extracting emotions labels and scores from the response
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    # Extract the top scoring (dominant) emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+    # Add the dominant emotion to the return dict.
+    emotions.update({'dominant_emotion': dominant_emotion})
 
-    return response.text
+    return emotions
